5ff689ebb3fbe78df23263e7908b700cba422b8e
Furukawa Yuma <hy4327.ttt@gmail.com>
2026-09-06 23:18:00 +0900

    csvを読み込めるように変更

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 #ファイルやフォルダのpathを読み込む
 
 from dotenv import load_dotenv
-from agent_framework import Agent, SkillsProvider
+from agent_framework import Agent, SkillsProvider,ToolApprovalMiddleware
 from agent_framework.openai import OpenAIChatClient
 
 
@@ -21,7 +21,14 @@ async def main() -> None:
     skills_provider = SkillsProvider.from_paths(
     #フォルダからSKiLLを見つけ変数に格納
         skill_paths=BASE_DIR / "skills",
-    #検索するフォルダ、ファイルを指定
+    # 開発中はSKILL.mdやCSVの変更を毎回反映させる
+        disable_caching=True,
+    )
+
+    approval_middleware = ToolApprovalMiddleware(
+        auto_approval_rules=[
+            SkillsProvider.read_only_tools_auto_approval_rule
+        ],
     )
 
     # OpenAIを利用するエージェントを作成
@@ -29,14 +36,18 @@ async def main() -> None:
         client=OpenAIChatClient(),
         name="DentalCareAgent",
         instructions=(
-            "あなたは日本語で回答するアシスタントです。"
-            "ユーザーの質問に関連するSkillがある場合は、"
-            "必ずそのSkillを読み込んで、Skillの指示に従って回答してください。"
+        "あなたは日本語で回答するアシスタントです。"
+        "質問に関連するSkillがある場合は、必ずそのSkillを読み込んでください。"
+        "Skillに参照ファイルが指定されている場合は、"
+        "回答を作る前にread_skill_resourceでそのファイルを読み込んでください。"
+        "回答は読み込んだ資料を根拠に作成してください。"
         ),
         context_providers=[skills_provider],
     #skill providerをAgentに渡す
+         middleware=[approval_middleware],
     )
-
+# Agentの会話・ツール実行状態を保存するセッションを作る
+    session = agent.create_session()
     question = "虫歯にならないためには？"
     #質問を変数の保存
     print(f"質問：{question}")
@@ -44,11 +55,12 @@ async def main() -> None:
     print("回答を生成しています...\n")
     #処理中のメッセージを表示
 
-    result = await agent.run(question)
+    result = await agent.run(question,session=session)
     #非同期処理の結果が返ってくるまで待機する
 
+
     print("回答：")
-    print(result)
+    print(result.text)
     #生成された回答を表示
 
 
